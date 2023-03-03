e7ce79f27806daad00058b962ec1a3ab88d1fef9
Fasika Dagnachew <fasika1515@gmail.com>
2023-03-03 15:14:55 +0300

    commit

--- app/head.tsx
+++ app/head.tsx
@@ -7,20 +7,20 @@ export default function Head() {
       <meta name="keywords" content="Ethiopia, agro industry, products, services, sustainable agriculture" />
       <meta name="author" content="Aspire Ethiopia" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="canonical" href="https://www.aspireethiopia.com/" />
+      <link rel="canonical" href="https://aspire-eth.vercel.app/" />
       {/* <!-- Add Open Graph tags for social media sharing --> */}
       <meta property="og:title" content="Aspire Ethiopia | Modern Agro Industry Products and Services" />
       <meta property="og:description" content="Creating an abundant Ethiopia with wonderful and modern agro industry products and services" />
-      <meta property="og:image" content="https://picsum.photos/100" />
-      <meta property="og:url" content="https://www.aspireethiopia.com/" />
+      <meta property="og:image" content="https://aspire-eth.vercel.app/images/logo/Aspire_Ethiopia_-_Hor_Grad_BG_White.png" />
+      <meta property="og:url" content="https://aspire-eth.vercel.app/" />
       <meta property="og:type" content="website" />
       {/* <!-- Add Twitter Card tags for social media sharing --> */}
       <meta name="twitter:title" content="Aspire Ethiopia | Modern Agro Industry Products and Services" />
       <meta name="twitter:description" content="Creating an abundant Ethiopia with wonderful and modern agro industry products and services" />
-      <meta name="twitter:image" content="https://picsum.photos/100" />
+      <meta name="twitter:image" content="https://aspire-eth.vercel.app/images/logo/Aspire_Ethiopia_-_Hor_Grad_BG_White.png" />
       <meta name="twitter:card" content="summary_large_image" />
       {/* <!-- Add Favicon --> */}
-      <link rel="icon" type="image/png" sizes="32x32" href="https://picsum.photos/100" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/icon.png" />
     </>
   )
 }
