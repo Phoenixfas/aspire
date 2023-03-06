c8df2ff44bcc15af5532a36d13876a3bc9df5458
Fasika Dagnachew <fasika1515@gmail.com>
2023-03-06 08:14:26 +0300

    test

---
+++ app/Test.tsx
@@ -0,0 +1,255 @@
+import React from 'react'
+
+export default function Test() {
+  return (
+    <div style={{backgroundColor: "#00a350"}}>
+        <div className="page-home__container">
+
+        <div className="split-artpieces js-split-artpieces">
+        <div className="hover-trigger-top js-trigger-top" data-pos="top"></div>
+        <div className="hover-trigger-bottom js-trigger-bottom" data-pos="bottom"></div>
+
+
+
+
+        <div className="split-artpieces__wrapper js-artpieces-wrapper is-active has-video" >
+        <div className="split-artpieces__image split-artpieces__image--top js-artpiece-top" data-section="0" >
+
+        <div className="split-artpieces__curtain">
+            <div className="split-artpieces__src is-loaded" data-component="lazy" data-imgsrc="[{&quot;width&quot;:300,&quot;height&quot;:225,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2020\/01\/video-top-300x0-c-default.jpg&quot;},{&quot;width&quot;:500,&quot;height&quot;:374,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2020\/01\/video-top-500x0-c-default.jpg&quot;},{&quot;width&quot;:700,&quot;height&quot;:524,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2020\/01\/video-top-700x0-c-default.jpg&quot;}]" ></div>
+        </div>
+        </div>
+
+        <div className="split-artpieces__image split-artpieces__image--bottom" data-section="0" >
+
+        <div className="split-artpieces__curtain">
+            <div className="split-artpieces__src is-loaded" data-component="lazy" data-imgsrc="[{&quot;width&quot;:300,&quot;height&quot;:225,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2020\/01\/video-bottom-300x0-c-default.jpg&quot;},{&quot;width&quot;:500,&quot;height&quot;:374,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2020\/01\/video-bottom-500x0-c-default.jpg&quot;},{&quot;width&quot;:700,&quot;height&quot;:524,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2020\/01\/video-bottom-700x0-c-default.jpg&quot;}]" ></div>
+        </div>
+        </div>
+
+        <div id="video-modal-initial-cont" className="video-modal">
+        <div className="video-modal__cta js-animate-in-view is-in-view" data-component="modal-button">
+        <div className="video-modal__icon js-toggle-modal" data-target="#video-modal-0">
+        <svg viewBox="0 0 40 40" enable-background="new 0 0 40 40"  className="" preserveAspectRatio="none">
+        <use x="0" y="0" href="#play-black" ></use>
+        </svg>
+        </div>
+        <p className="video-modal__caption">Play video about the Frans Hals Museum</p>    </div>
+
+        <div id="video-modal-0" className="video-modal__container">
+        <div className="animation-slides animation-slides--video-modal">
+        <div className="animation-slides__top">
+        <div className="video-modal__cover-wrapper">
+            <div className="video-modal__cover" ></div>
+        </div>
+        </div>
+        <div className="animation-slides__bottom">
+        <div className="video-modal__cover-wrapper">
+            <div className="video-modal__cover" ></div>
+        </div>
+        </div>
+        </div>
+
+        <div className="video-modal__content">
+        <div className="video-modal__close" data-component="modal-button">
+        <span className="video-modal__close-label js-toggle-modal underline" data-target="#video-modal-0">Close</span>
+        </div>
+
+        <div className="video-modal__video">
+        <div className="native-video" data-component="native-video">
+        <video className="native-video__asset" data-md-src="https://www.franshalsmuseum.nl/wp-content/uploads/2020/02/final_fullvideo_FransHalsinHaarlem_DEF4.mp4" data-xs-src="https://www.franshalsmuseum.nl/wp-content/uploads/2020/02/final_fullvideo_FransHalsinHaarlem_DEF4.mp4" controls data-loaded="false"></video>
+        </div>            </div>
+        </div>
+        </div>
+        </div>                    </div><div className="split-artpieces__wrapper js-artpieces-wrapper" >
+        <div className="split-artpieces__image split-artpieces__image--top js-artpiece-top" data-section="1" >
+
+        <div className="split-artpieces__curtain">
+            <div className="split-artpieces__src is-loaded" data-component="lazy" data-imgsrc="[{&quot;width&quot;:300,&quot;height&quot;:224,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2017\/08\/mf-2006-78_1-300x0-c-default.jpg&quot;},{&quot;width&quot;:500,&quot;height&quot;:373,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2017\/08\/mf-2006-78_1-500x0-c-default.jpg&quot;},{&quot;width&quot;:700,&quot;height&quot;:522,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2017\/08\/mf-2006-78_1-700x0-c-default.jpg&quot;}]"></div>
+        </div>
+        </div>
+
+        <div className="split-artpieces__image split-artpieces__image--bottom" data-section="1" >
+                            <div data-href="https://www.franshalsmuseum.nl/en/art/portret-van-een-dame-met-handschoenen/" className="split-artpieces__artpiece js-artpiece">
+                
+
+        <div className="intrinsic is-loaded" data-component="lazy" data-imgsrc="[{&quot;width&quot;:500,&quot;height&quot;:619,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2017\/08\/os-2011-7-Velsink-2011-500x0-c-default.jpg&quot;},{&quot;width&quot;:800,&quot;height&quot;:990,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2017\/08\/os-2011-7-Velsink-2011-800x0-c-default.jpg&quot;},{&quot;width&quot;:1200,&quot;height&quot;:1485,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2017\/08\/os-2011-7-Velsink-2011-1200x0-c-default.jpg&quot;}]" >
+        </div>
+
+            </div>
+
+        <div className="split-artpieces__curtain">
+            <div className="split-artpieces__src is-loaded" data-component="lazy" data-imgsrc="[{&quot;width&quot;:300,&quot;height&quot;:224,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2017\/08\/Layer-5-300x0-c-default.jpg&quot;},{&quot;width&quot;:500,&quot;height&quot;:373,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2017\/08\/Layer-5-500x0-c-default.jpg&quot;},{&quot;width&quot;:700,&quot;height&quot;:523,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2017\/08\/Layer-5-700x0-c-default.jpg&quot;}]" ></div>
+        </div>
+        </div>
+
+            </div><div className="split-artpieces__wrapper js-artpieces-wrapper is-active" >
+        <div className="split-artpieces__image split-artpieces__image--top js-artpiece-top" data-section="2" >
+
+        <div className="split-artpieces__curtain">
+            <div className="split-artpieces__src is-loaded" data-component="lazy" data-imgsrc="[{&quot;width&quot;:300,&quot;height&quot;:224,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2018\/02\/FransHals_Mashups_1-1-300x0-c-default.jpg&quot;},{&quot;width&quot;:500,&quot;height&quot;:373,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2018\/02\/FransHals_Mashups_1-1-500x0-c-default.jpg&quot;},{&quot;width&quot;:700,&quot;height&quot;:522,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2018\/02\/FransHals_Mashups_1-1-700x0-c-default.jpg&quot;}]" ></div>
+        </div>
+        </div>
+
+        <div className="split-artpieces__image split-artpieces__image--bottom" data-section="2" >
+                            <div data-href="https://www.franshalsmuseum.nl/en/art/the-licentious-kitchen-maid/" className="split-artpieces__artpiece js-artpiece">
+                
+
+        <div className="intrinsic is-loaded" data-component="lazy" data-imgsrc="[{&quot;width&quot;:500,&quot;height&quot;:581,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2018\/03\/OS-I-295-500x0-c-default.png&quot;},{&quot;width&quot;:800,&quot;height&quot;:930,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2018\/03\/OS-I-295-800x0-c-default.png&quot;},{&quot;width&quot;:1200,&quot;height&quot;:1396,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2018\/03\/OS-I-295-1200x0-c-default.png&quot;}]">
+        </div>
+
+            </div>
+
+        <div className="split-artpieces__curtain">
+            <div className="split-artpieces__src is-loaded" data-component="lazy" data-imgsrc="[{&quot;width&quot;:300,&quot;height&quot;:224,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2018\/02\/FransHals_Mashups2-1-300x0-c-default.jpg&quot;},{&quot;width&quot;:500,&quot;height&quot;:373,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2018\/02\/FransHals_Mashups2-1-500x0-c-default.jpg&quot;},{&quot;width&quot;:700,&quot;height&quot;:522,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2018\/02\/FransHals_Mashups2-1-700x0-c-default.jpg&quot;}]" ></div>
+        </div>
+        </div>
+
+            </div></div>
+
+        <section className="home-discover js-home-section" data-color="#E57B54"> 					
+        <div className="home-discover__inner">
+        <div className="home-highlighted-section__header js-animate-in-view is-in-view">
+        <div className="">
+        <h2 className="home-highlighted-section__title">                
+        <div className="original-title js-original-title">Locations</div>
+
+        <span className="home-highlighted-section__title--half">
+        <span className="js-page-title">Locations</span>
+        </span>
+        <span className="home-highlighted-section__title--half">
+        <span className="js-page-title">Locations</span>
+        </span>
+        </h2>
+
+        </div>
+        </div>
+
+
+
+        <div className="locations__container locations__container--discover">
+
+        <div className="locations__wrapper">
+            <div className="locations__single">
+            
+        <svg viewBox="0 0 221.8 113.7" enable-background="new 0 0 221.8 113.7" preserveAspectRatio="none">
+        <use x="0" y="0" href="#hof-outline" ></use>
+        </svg>
+            
+        <span className="locations__address">
+            <strong>HOF</strong><br/>
+            Groot Heiligland 62<br/> 2011 ES Haarlem
+        </span>
+
+                    </div>
+
+            <div className="locations__single">
+            
+                <svg viewBox="0 0 80.6 120.5" enable-background="new 0 0 80.6 120.5"  className="" preserveAspectRatio="none">
+        <use x="0" y="0" href="#hal-outline"></use>
+        </svg>
+            
+            <span className="locations__address">
+                <strong>HAL</strong><br/>
+                Grote Markt 16<br/>
+        2011 RD Haarlem
+            </span>
+
+                    </div>
+
+        </div>
+
+        <div className="locations__title">One museum two locations</div>
+        <div className="locations__copy">Welcome to the Frans Hals Museum - The art museum of Haarlem - 
+        With art from the 16th and 17th centuries, modern and new art and a stimulating mix of both.</div>
+        <a href="https://www.franshalsmuseum.nl/en/visit/practical-info/hof/" className="locations__link active-line">
+        About our buildings
+        <span></span>
+        </a>
+        </div></div>				</section>
+
+        <section className="home-journal js-home-section" data-color="#9ACF8A"> 					<div className="home-journal__inner js-animate-in-view">
+        <div className="home-highlighted-section__header js-animate-in-view">
+        <div className="">
+        <h2 className="home-highlighted-section__title">                
+        <div className="original-title js-original-title">News</div>
+
+        <span className="home-highlighted-section__title--half">
+        <span className="js-page-title">News</span>
+        </span>
+        <span className="home-highlighted-section__title--half">
+        <span className="js-page-title">News</span>
+        </span>
+        </h2>
+
+        <p><a href="https://www.franshalsmuseum.nl/en/learn/news/" className="active-line">Explore museum highlights and more<span></span></a><span className="animation-icon"><span className="static-svg"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 43.5 41" >
+        <path d="M11.8,21.7V4.1c0-1.3,1.1-2.3,2.4-2.3h25.2c1.3,0,2.4,1,2.4,2.3V37c0,1.3-1.1,2.3-2.4,2.3H13.6H6.8
+        c-5,0-5-5-5-5V30V19.2h10v15c0,2.8-2.2,5-5,5"></path>
+        <path d="M35,19.2H19.2V8.4H35V19.2z"></path>
+        <path d="M35,25.9H18.5"></path>
+        </svg>
+        </span><span className="animation-svg" data-component="animation-icon"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 43.5 41" >
+        <path d="M11.8,21.7V4.1c0-1.3,1.1-2.3,2.4-2.3h25.2c1.3,0,2.4,1,2.4,2.3V37c0,1.3-1.1,2.3-2.4,2.3H13.6H6.8
+        c-5,0-5-5-5-5V30V19.2h10v15c0,2.8-2.2,5-5,5" ></path>
+        <path d="M35,19.2H19.2V8.4H35V19.2z" ></path>
+        <path d="M35,25.9H18.5" ></path>
+        </svg>
+        </span></span></p>    </div>
+        </div>
+
+
+        <div className="home-journal__news-cards js-animate-in-view">
+                        
+        <div className="news-card bordered js-animate-in-view"  data-color="rgb(252,227,115)">
+        <a className="news-card__link news-card__thumbnail" href="https://www.franshalsmuseum.nl/en/news/hof-temporarily-closed-from-january-10-hal-remains-open/" >
+            
+
+        <div className="intrinsic  bordered intrinsic--split" data-component="lazy" data-imgsrc="[{&quot;width&quot;:300,&quot;height&quot;:200,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2022\/09\/07.-Frans-Hals-Museum-locatie-HOF-Foto-Stefan-Rustenburg-scaled-300x0-c-default.jpg&quot;},{&quot;width&quot;:500,&quot;height&quot;:334,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2022\/09\/07.-Frans-Hals-Museum-locatie-HOF-Foto-Stefan-Rustenburg-scaled-500x0-c-default.jpg&quot;},{&quot;width&quot;:700,&quot;height&quot;:467,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2022\/09\/07.-Frans-Hals-Museum-locatie-HOF-Foto-Stefan-Rustenburg-scaled-700x0-c-default.jpg&quot;}]" >
+        <div className="intrinsic__half intrinsic__half--top">
+        <div className="intrinsic__half__image" ></div>
+        </div>
+        <div className="intrinsic__half intrinsic__half--bottom">
+        <div className="intrinsic__half__image"></div>
+        </div>	
+        </div>
+
+        <div className="news-card__inner"></div>
+        </a>
+        <div className="news-card__infos">
+
+        <p className="news-card__post-date"><a href="https://www.franshalsmuseum.nl/en/news/hof-temporarily-closed-from-january-10-hal-remains-open/"><span>News</span> 20th December 2022</a></p>
+        <a className="news-card__link" href="https://www.franshalsmuseum.nl/en/news/hof-temporarily-closed-from-january-10-hal-remains-open/">
+        <h2 className="news-card__title">HOF temporarily closed from 10 January, HAL open, tickets half price</h2>
+        </a>
+        <p className="news-card__excerpt"><a href="https://www.franshalsmuseum.nl/en/news/hof-temporarily-closed-from-january-10-hal-remains-open/"></a></p>
+        </div>
+        </div>                                
+        <div className="news-card bordered js-animate-in-view"  data-color="rgb(252,227,115)">
+        <a className="news-card__link news-card__thumbnail" href="https://www.franshalsmuseum.nl/en/news/photo-report-unveiling-naked-city/" >
+            
+
+        <div className="intrinsic  bordered intrinsic--split" data-component="lazy" data-imgsrc="[{&quot;width&quot;:300,&quot;height&quot;:200,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2022\/12\/Hotel-Arena-Haarlem_Anna-Witkowska-57-scaled-300x0-c-default.jpg&quot;},{&quot;width&quot;:500,&quot;height&quot;:334,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2022\/12\/Hotel-Arena-Haarlem_Anna-Witkowska-57-scaled-500x0-c-default.jpg&quot;},{&quot;width&quot;:700,&quot;height&quot;:467,&quot;src&quot;:&quot;https:\/\/www.franshalsmuseum.nl\/wp-content\/uploads\/2022\/12\/Hotel-Arena-Haarlem_Anna-Witkowska-57-scaled-700x0-c-default.jpg&quot;}]" >
+        <div className="intrinsic__half intrinsic__half--top">
+        <div className="intrinsic__half__image" ></div>
+        </div>
+        <div className="intrinsic__half intrinsic__half--bottom">
+        <div className="intrinsic__half__image" ></div>
+        </div>	
+        </div>
+
+        <div className="news-card__inner"></div>
+        </a>
+        <div className="news-card__infos">
+
+        <p className="news-card__post-date"><a href="https://www.franshalsmuseum.nl/en/news/photo-report-unveiling-naked-city/"><span>News</span> 12th December 2022</a></p>
+        <a className="news-card__link" href="https://www.franshalsmuseum.nl/en/news/photo-report-unveiling-naked-city/">
+        <h2 className="news-card__title">Photo report unveiling Naked City</h2>
+        </a>
+        <p className="news-card__excerpt"><a href="https://www.franshalsmuseum.nl/en/news/photo-report-unveiling-naked-city/"></a></p>
+        </div>
+        </div>            </div>
+        </div>				</section>
+        </div>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import Hero from './Hero'
 import InfiniteAutoScroller from './Scroller'
+import Test from './Test'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Hero />
       <InfiniteAutoScroller />
+      <Test />
     </main>
   )
 }
